1a355d4f46f1abd8ffe1b68b14f594feef91fd52
ノグソン <nogson@MacBook-Pro-3.local>
2018-03-18 23:57:21 +0900

    no message

--- client.js
+++ client.js
@@ -1,7 +1,7 @@
 global.THREE = require('three');
 const createBackground = require('three-vignette-background');
 const Stats = require('stats.js');
-const glslify = require('glslify')
+const glslify = require('glslify');
 
 const particlesVertex = glslify('./src/js/shaders/particles/vertexShader.vert');
 const particlesFragment = glslify('./src/js/shaders/particles/fragmentShader.frag');
@@ -13,187 +13,142 @@ const PARTICLE_COUNT = Math.pow(2, 20);
 const PARTICLE_TEXTURE_SIZE = Math.sqrt(PARTICLE_COUNT);
 
 let time = 0.0;
-const WW = window.innerWidth;
-let WH = window.innerHeight;
-let ASPECT = WW / WH;
 
 if (!THREE.Math.isPowerOfTwo(PARTICLE_TEXTURE_SIZE)) {
   throw new Error('Particle count should be a power of two.');
 }
 
-const PARTICLE_TEXTURE_RESOLUTION = new THREE.Vector2(
+const RESOLUTION = new THREE.Vector2(
   window.innerWidth,
   window.innerHeight
 );
-const PARTICLE_TEXTURE_RESOLUTION_HALF = PARTICLE_TEXTURE_RESOLUTION.clone().multiplyScalar(0.5);
 
-const SHADER_UNIFORMS_GLOBAL = {
-  resolution: {
-    type:'v2',
-    value: PARTICLE_TEXTURE_RESOLUTION,
-  },
-  velocityMax: {
-    type:'f',
-    value: Math.min(
-			PARTICLE_TEXTURE_RESOLUTION.x,
-			PARTICLE_TEXTURE_RESOLUTION.y
-		) * 0.125
-  },
-  colorBase: {
-    type:'c',
-    value: new THREE.Color('hsl(245, 100%, 30%)'),
-  },
-  colorIntense: {
-    type:'c',
-    value: new THREE.Color('hsl(15, 100%, 30%)'),
-  },
-  time: {
-    type:'f',
-    value: 0,
-  },
-  delta: {
-    type:'f',
-    value: 0,
-  }
-};
-
-// const camera = new THREE.OrthographicCamera(
-//   -PARTICLE_TEXTURE_RESOLUTION_HALF.x,
-//   PARTICLE_TEXTURE_RESOLUTION_HALF.x,
-//   PARTICLE_TEXTURE_RESOLUTION_HALF.y,
-//   -PARTICLE_TEXTURE_RESOLUTION_HALF.y,
-//   1,
-//   CAMERA_DEPTH
-// );
-// camera.position.z = CAMERA_DEPTH / 2;
-
-// const renderer = new THREE.WebGLRenderer();
+const RESOLUTION_HALF = RESOLUTION.clone().multiplyScalar(0.5);
 
 
-const app = {
-  renderer: new THREE.WebGLRenderer(),
-  scene: new THREE.Scene(),
-  camera: new THREE.PerspectiveCamera(60, WW / WH, 0.1, CAMERA_DEPTH)
-};
+
+//基本設定
+const renderer = new THREE.WebGLRenderer();
+const scene = new THREE.Scene()
+
+const camera = new THREE.OrthographicCamera(
+  RESOLUTION_HALF.x * -1, //left 視線のどれぐらい左まで画面に入れるか。 
+  RESOLUTION_HALF.x, //right 視線のどれぐらい右まで画面に入れるか。 
+  RESOLUTION_HALF.y, //top 視線のどれぐらい上まで画面に入れるか。
+  RESOLUTION_HALF.y * -1, //bottom 視線のどれぐらい下まで画面に入れるか。 
+  1, //near
+  CAMERA_DEPTH //far
+);
+camera.position.z = CAMERA_DEPTH / 2;
+
 const body = document.getElementsByTagName('body')[0];
 
-//app.renderer.setClearColor(new THREE.Color(0xffffff), 1.0);
-app.renderer.setPixelRatio(window.devicePixelRatio || 1);
-app.renderer.setSize(PARTICLE_TEXTURE_RESOLUTION.x, PARTICLE_TEXTURE_RESOLUTION.y);
+//renderer.setClearColorはしない
+renderer.setPixelRatio(window.devicePixelRatio || 1);
+renderer.setSize(RESOLUTION.x, RESOLUTION.y);
 
 // canvasをbodyに追加
-body.appendChild(app.renderer.domElement);
-
-app.camera.position.z = CAMERA_DEPTH / 2;
+body.appendChild(renderer.domElement);
 
 
 function getParticleData(particleCount, textureSize) {
   const data = new Float32Array(particleCount * 4);
-  const getRandomValue = () => Math.random() - 0.5;
-  
+
   for (let i = 0; i < data.length; i += 4) {
     const position = new THREE.Vector2(
-      getRandomValue(),
-      getRandomValue()
+      Math.random() - 0.5,
+      Math.random() - 0.5
     );
-    
+
     data[i + 0] = position.x;
     data[i + 1] = position.y;
   }
-  
+
   const texture = new THREE.DataTexture(
     data, textureSize, textureSize,
-		THREE.RGBAFormat,
-		THREE.FloatType,
-		THREE.Texture.DEFAULT_MAPPING,
-		THREE.RepeatWrapping,
-		THREE.RepeatWrapping,
-		THREE.NearestFilter,
-		THREE.NearestFilter
-	);
+    THREE.RGBAFormat,
+    THREE.FloatType,
+    THREE.Texture.DEFAULT_MAPPING,
+    THREE.RepeatWrapping,
+    THREE.RepeatWrapping,
+    THREE.NearestFilter,
+    THREE.NearestFilter
+  );
 
-	texture.needsUpdate = true;
+  texture.needsUpdate = true;
 
-	return texture;
+  return texture;
 }
 
-function getPlane(size) {
-  const geometry = new THREE.PlaneGeometry(size.x, size.y, 1, 1);
-  const material = new THREE.MeshBasicMaterial();
-  
-  return new THREE.Mesh(geometry, material);
-}
-
-
-  
-
-
-
-
-
-
-/**
- * Prepare tools to render to texture
- */
 
 var renderTarget = new THREE.WebGLRenderTarget(
   PARTICLE_TEXTURE_SIZE,
-  PARTICLE_TEXTURE_SIZE,
-  {
-		wrapS: THREE.ClampToEdgeWrapping,
-		wrapT: THREE.ClampToEdgeWrapping,
-		minFilter: THREE.NearestFilter,
-		magFilter: THREE.NearestFilter,
-		format: THREE.RGBAFormat,
-		type: THREE.FloatType,
-		stencilBuffer: false,
-	}
+  PARTICLE_TEXTURE_SIZE, {
+    minFilter: THREE.NearestFilter,
+    magFilter: THREE.NearestFilter,
+    type: THREE.FloatType,
+    stencilBuffer: false,//ステンシルバッファをOFF
+  }
 );
 
-const textureBuffers = new (function () {
+const textureBuffers = new(function () {
   this.in = renderTarget;
   this.out = renderTarget.clone();
-  
+
   this.swap = () => {
     [this.in, this.out] = [this.out, this.in];
   };
 });
 
-const rttScene = new THREE.Scene();
-const rttPlane = getPlane(PARTICLE_TEXTURE_RESOLUTION);
-rttScene.add(rttPlane);
 
-const renderToTexture = (shader, inTexture, outTexture) => {
-	console.log(shader,inTexture);
-	
-  rttPlane.material = shader;
-  rttPlane.material.uniforms.tData.value = inTexture;
-  app.renderer.render(rttScene, camera, outTexture, true);
+
+
+const SHADER_UNIFORMS_GLOBAL = {
+  tData: {
+    type: 't',
+    value: getParticleData(PARTICLE_COUNT, PARTICLE_TEXTURE_SIZE)
+  },
+  resolution: {
+    type: 'v2',
+    value: RESOLUTION,
+  },
+  velocityMax: {
+    type: 'f',
+    value: Math.min(
+      RESOLUTION.x,
+      RESOLUTION.y
+    ) * 0.125
+  },
+  colorBase: {
+    type: 'c',
+    value: new THREE.Color('hsl(245, 100%, 30%)'),
+  },
+  colorIntense: {
+    type: 'c',
+    value: new THREE.Color('hsl(15, 100%, 30%)'),
+  },
+  time: {
+    type: 'f',
+    value: 0,
+  },
+  delta: {
+    type: 'f',
+    value: 0,
+  }
 };
 
-/**
- * Create and mutate base data
- */
+const rttScene = new THREE.Scene();
+const rttGeometry = new THREE.PlaneGeometry(RESOLUTION.x, RESOLUTION.y, 1, 1);
 
-// const SHADER_PART_NOISE = document.getElementById('shader-noise-perlin').textContent;
-
-const baseData = getParticleData(
-  PARTICLE_COUNT,
-  PARTICLE_TEXTURE_SIZE
-);
-const simulationShader = new THREE.ShaderMaterial({
-  uniforms: Object.assign({}, SHADER_UNIFORMS_GLOBAL, {
-    tData: { 
-      type:'t',
-      value: baseData
-     },
-  }),
+const rttMaterial = new THREE.ShaderMaterial({
+  uniforms: SHADER_UNIFORMS_GLOBAL,
   vertexShader: simulationVertex,
   fragmentShader: simulationFragment,
 });
 
-rttPlane.material = simulationShader;
-app.renderer.render(rttScene, app.camera, textureBuffers.out, true);
+const rttPlane = new THREE.Mesh(rttGeometry, rttMaterial);
+rttScene.add(rttPlane);
+renderer.render(rttScene, camera, textureBuffers.out, true);
 
 /**
  * Mutate and render GPU texture
@@ -203,15 +158,15 @@ const vertices = new Float32Array(PARTICLE_COUNT * 3);
 
 
 for (let i = 0; i < vertices.length; i++) {
-  vertices[i] = Math.random();
+  vertices[i] = 0;
 }
 
 const uvs = new Float32Array(PARTICLE_COUNT * 2);
 
 for (let i = 0; i < uvs.length; i += 2) {
-	const index = i / 2;
-	uvs[i + 0] = (index % PARTICLE_TEXTURE_SIZE) / PARTICLE_TEXTURE_SIZE;
-	uvs[i + 1] = Math.floor(index / PARTICLE_TEXTURE_SIZE) / PARTICLE_TEXTURE_SIZE;
+  const index = i / 2;
+  uvs[i + 0] = (index % PARTICLE_TEXTURE_SIZE) / PARTICLE_TEXTURE_SIZE;
+  uvs[i + 1] = Math.floor(index / PARTICLE_TEXTURE_SIZE) / PARTICLE_TEXTURE_SIZE;
 }
 
 const particleGeometry = new THREE.BufferGeometry();
@@ -219,27 +174,32 @@ particleGeometry.addAttribute(
   'position',
   new THREE.BufferAttribute(vertices, 3)
 );
-particleGeometry.addAttribute('uv', new THREE.BufferAttribute(uvs, 2));
+particleGeometry.addAttribute(
+  'uv', 
+  new THREE.BufferAttribute(uvs, 2)
+);
+
+const particleMaterial = new THREE.ShaderMaterial({
+  uniforms: Object.assign({}, SHADER_UNIFORMS_GLOBAL, {
+    tData: {
+      type: 't',
+      value: textureBuffers.in
+    },
+  }),
+
+  vertexShader: particlesVertex,
+  fragmentShader: particlesFragment,
+  depthTest: false,
+  transparent: true,
+  blending: THREE.AdditiveBlending,
+})
+
 
 const particles = new THREE.Points(
   particleGeometry,
-  new THREE.ShaderMaterial({
-    uniforms: Object.assign({}, SHADER_UNIFORMS_GLOBAL, {
-    tData: { 
-      type:'t',
-      value: textureBuffers.in 
-    },
-  }),
-    
-    vertexShader: particlesVertex,
-    fragmentShader: particlesFragment,
-    depthTest: false,
-    transparent: true,
-    blending: THREE.AdditiveBlending,
-  })
+  particleMaterial
 );
 
-const scene = new THREE.Scene();
 scene.add(particles);
 
 const start = Date.now() / 1000;
@@ -247,27 +207,26 @@ let previous = start;
 
 const render = () => {
   requestAnimationFrame(render);
-  
+
   textureBuffers.swap();
-  
+
   const now = Date.now() / 1000;
   const delta = now - previous;
   const elapsed = now - start;
-  
-  simulationShader.uniforms.time.value = elapsed;
-  simulationShader.uniforms.delta.value = delta;
-  
-  simulationShader.uniforms.tData.value = textureBuffers.in.texture;
-  app.renderer.render(rttScene, app.camera, textureBuffers.out, true);
-  
+
+  rttMaterial.uniforms.time.value = elapsed;
+  rttMaterial.uniforms.delta.value = delta;
+
+  rttMaterial.uniforms.tData.value = textureBuffers.in.texture;
+  renderer.render(rttScene, camera, textureBuffers.out, true);
+
   particles.material.uniforms.time.value = elapsed;
   particles.material.uniforms.delta.value = delta;
-  
+
   particles.material.uniforms.tData.value = textureBuffers.in.texture;
-  app.renderer.render(scene, app.camera);
-  
+  renderer.render(scene, camera);
+
   previous = now;
 };
 
 render();
-//document.body.appendChild(renderer.domElement);
